d240c2ff3f5ded1c3be38d0fde30d7b09df4eccc
AimanYosofiUlt <101520036+AimanYosofiUlt@users.noreply.github.com>
2025-04-25 10:36:04 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,7 @@
 fun main() {
     println("Hello world")
     println("Hi Again")
-    println("Hi Again2 5")
-    println("Hi Again 6")
     println("Hi Again 7")
-    println("Change directly from Githup")
     println("Another chnage from Githup")
+    pringln("Hi Aiman")
 }
